bd3d60c01038910a160e424232f7e746219e1e61
Mirco Dotta <mirco.dotta@gmail.com>
2014-04-12 17:00:11 +0200

    Removed blocking code (refactoring)
    Blocking code is not a best practice when using Spray, Akka and Scala Futures.
    Hence, I've refactored the code to remove both the CountDownLatch and the Await
    on Future.
    
    Furthermore, messages exchanged by actors should always be *immutable*
    (otherwise, thread-safety is not guaranteed). This was not the case before this
    refactoring, as the `AggregateActor` was sending a mutable collection back upon
    reception of the `Result` message.

--- src/main/scala/br/com/strategiatec/spray/hanacloud/WordCountService.scala
+++ src/main/scala/br/com/strategiatec/spray/hanacloud/WordCountService.scala
@@ -1,33 +1,53 @@
 package br.com.strategiatec.spray.hanacloud
 
 import scala.collection.mutable.ArrayBuffer
-import java.util.concurrent.CountDownLatch
+
 import akka.actor.Actor
-import scala.collection.mutable.HashMap
 import akka.actor.Props
 import akka.routing.RoundRobinRouter
+import spray.json._
+import spray.json.DefaultJsonProtocol._
+import spray.routing.RequestContext
 
-class MasterActor(latch: CountDownLatch) extends Actor {
-  val mapActor = context.actorOf(Props[MapActor].withRouter(RoundRobinRouter(nrOfInstances =5)), name = "map")
-  val reduceActor = context.actorOf(Props[ReduceActor].withRouter(RoundRobinRouter(nrOfInstances = 5)), name = "reduce")
-  val aggregateActor = context.actorOf(Props(new AggregateActor(latch)), name = "aggregate")
-  
+class MasterActor(ctx: RequestContext) extends Actor {
+  import MasterActor._
   def receive: Receive = {
-    case line: String => mapActor ! line
-    case mapData: MapData => reduceActor ! mapData
-    case reduceData: ReduceData => aggregateActor ! reduceData
-    case Result => aggregateActor forward Result
+    case Process(lines) => context.actorOf(Props(new MapReduceActor(lines)))
+    case Result(value)  => ctx.complete { value.toJson.prettyPrint }
   }
+}
+
+object MasterActor {
+  sealed trait Msg 
+  case class Process(lines: List[String]) extends Msg
+}
+
+class MapReduceActor(lines: List[String]) extends Actor {
+  val mapActor = context.actorOf(Props[MapActor].withRouter(RoundRobinRouter(nrOfInstances = 5)), name = "map")
+  val reduceActor = context.actorOf(Props[ReduceActor].withRouter(RoundRobinRouter(nrOfInstances = 5)), name = "reduce")
+  val aggregateActor = context.actorOf(Props(new AggregateActor), name = "aggregate")
+
+  var processedLines = 0
+
+  lines foreach (mapActor ! _)
+
+  def receive: Receive = {
+    case mapData: MapData       => reduceActor ! mapData
+    case reduceData: ReduceData => aggregateActor ! reduceData
+    case Aggregated             =>
+      processedLines += 1
+      if (areAllLinesProcessed) aggregateActor ! GetResult
+  }
+  
+  def areAllLinesProcessed: Boolean = processedLines == lines.size
 }
 
 class MapActor extends Actor {
   val STOP_WORDS = List("a", "am", "an", "and", "are", "as", "at",
     "be", "do", "go", "if", "in", "is", "it", "of", "on", "the", "to")
 
-  
   def receive: Receive = {
-    case message: String =>
-    sender ! countWords(message)
+    case message: String => sender ! countWords(message)
   }
 
   def countWords(line: String): MapData = MapData {
@@ -41,15 +61,15 @@ class MapActor extends Actor {
   }
 }
 
-class AggregateActor(latch: CountDownLatch) extends Actor {
-  val finalReducedMap = new HashMap[String, Int]
+class AggregateActor extends Actor {
+  val finalReducedMap = new collection.mutable.HashMap[String, Int]
 
   def receive: Receive = {
     case ReduceData(reduceDataMap) =>
       aggregateInMemoryReduce(reduceDataMap)
-      latch.countDown()
-    case Result => 
-      sender ! finalReducedMap
+      sender ! Aggregated
+    case GetResult                 =>
+      sender ! Result(finalReducedMap.toMap)
   }
 
   def aggregateInMemoryReduce(reducedList: Map[String, Int]): Unit = {
@@ -63,24 +83,25 @@ class AggregateActor(latch: CountDownLatch) extends Actor {
 }
 
 class ReduceActor extends Actor {
-	def receive: Receive = {
-	  case MapData(dataList) =>
-	    sender ! reduce(dataList)
-	}
-	
-	def reduce(words: IndexedSeq[WordCount]): ReduceData = ReduceData {
-	  words.foldLeft(Map.empty[String, Int]){
-	    (index, words) => 
-	      if (index contains words.word)
-	        index + (words.word -> (index.get(words.word).get + 1))
-	      else 
-	        index + (words.word -> 1)
-	  }
-	}
+  def receive: Receive = {
+    case MapData(dataList) => sender ! reduce(dataList)
+  }
+
+  def reduce(words: IndexedSeq[WordCount]): ReduceData = ReduceData {
+    words.foldLeft(Map.empty[String, Int]) {
+      (index, words) =>
+        if (index contains words.word)
+          index + (words.word -> (index.get(words.word).get + 1))
+        else
+          index + (words.word -> 1)
+    }
+  }
 }
 
 sealed trait MapReduceMessage
 case class WordCount(word: String, count: Int) extends MapReduceMessage
 case class MapData(dataList: ArrayBuffer[WordCount]) extends MapReduceMessage
 case class ReduceData(reduceDataMap: Map[String, Int]) extends MapReduceMessage
-case class Result() extends MapReduceMessage
+case object Aggregated extends MapReduceMessage
+case object GetResult extends MapReduceMessage
+case class Result(value: Map[String, Int]) extends MapReduceMessage
